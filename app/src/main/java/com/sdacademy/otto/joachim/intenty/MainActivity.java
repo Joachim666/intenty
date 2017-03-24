7a6b32ae2cd2216ecb59994b065b1b0612bc7d19
Joachim666 <joachimotto@wp.pl>
2017-03-24 17:29:51 +0100

    Initial commit

---
+++ app/src/main/java/com/sdacademy/otto/joachim/intenty/MainActivity.java
@@ -0,0 +1,81 @@
+package com.sdacademy.otto.joachim.intenty;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final int CAMERA_REQUEST_CODE = 1;
+
+
+    @BindView(R.id.mainActivityImageView)
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 0);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap image = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(image);
+
+
+        }
+    }
+
+    @OnClick(R.id.internetintent)
+    public void openBrowser() {
+        Intent internetIntent = new Intent(Intent.ACTION_VIEW);
+        internetIntent.setData(Uri.parse("http://google.pl"));
+        startActivity(internetIntent);
+    }
+
+    @OnClick(R.id.cameraintent)
+    public void takePhoto() {
+        Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (photoIntent.resolveActivity(getPackageManager()) != null) {
+
+            startActivityForResult(photoIntent, CAMERA_REQUEST_CODE);
+
+        }
+    }
+
+    @OnClick(R.id.phoneintent)
+    public void callMe() {
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:+4860932002"));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(callIntent);
+    }
+}
